6241f4652ab47bfb01cc42e2a60e23a03d631a41
asolodin <asolodin@comcast.net>
2023-11-26 13:19:42 -0600

    Changed data dir

--- seg_gen_test.py
+++ seg_gen_test.py
@@ -2,8 +2,8 @@ import cv2
 
 from seg_gen import SegGen
 
-gen = SegGen("liver/01/image00.png", "liver/01/mask00.png")
-img3 = gen.generate("liver/01/image68.png", "liver/01/mask68.png")
+gen = SegGen("data/liver/01/image00.png", "data/liver/01/mask00.png")
+img3 = gen.generate("data/liver/01/image68.png", "data/liver/01/mask68.png")
 cv2.namedWindow("img3", cv2.WINDOW_NORMAL)
 cv2.imshow("img3", img3)
 cv2.waitKey(0)
