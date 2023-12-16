ff1b61c05704c6062d4aee887bec043d60a4b3fe
Alex Wege <wegex009@umn.edu>
2023-12-16 11:48:08 -0600

    Initial Implementation of On-The-Fly SegGen

--- dataloader.py
+++ dataloader.py
@@ -40,7 +40,9 @@ class CobotLoaderBinary(Dataset):
     def __generate_aug(self, k, seed=False):
         assert 0 <= k <= 1.0
 
-        num = len(self.organ_ii)
+        print("Generate_Augs/k: ",k)
+        num = len(self.files)
+
         k_num = num * k
         perms = list(itertools.permutations(range(num), 2))
         pairs = random.sample(perms, int(k_num * k_num))
@@ -48,6 +50,8 @@ class CobotLoaderBinary(Dataset):
             rng = random.Random(42)
             pairs = rng.sample(perms, int(k_num * k_num))
         gens = dict()
+        print("Generate_Augs/perms: ",perms)
+        print("Generate_Augs/gens: ",gens)
 
         for p in pairs:
             i_0 = self.organ_ii[p[0]]
@@ -64,6 +68,25 @@ class CobotLoaderBinary(Dataset):
 
             mask_orig = cv2.imread(img_pair1[1], cv2.IMREAD_GRAYSCALE)
             self.__add_file(img, mask_orig)
+            
+    def __generate_aug_single(self, idx, weights=None):
+        #weights is an optional dictionary of (idx,weight) to set pair selection. None defaults to uniform, global selection
+        
+        img_pair1 = self.files[idx]
+        idx2 = np.random.choice(len(self.files),p=weights)
+        img_pair2 = self.files[idx2]
+        
+        gen = self.aug_gens.get(idx)
+        if gen is None:
+            gen = SegGen(img_pair1[0], img_pair1[1])
+            self.aug_gens[idx] = gen
+        img = gen.generate(img_pair2[0], img_pair2[1])
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if self.image_size is not None:
+            img = cv2.resize(img, self.image_size)
+        #mask_orig = cv2.imread(img_pair1[1], cv2.IMREAD_GRAYSCALE)
+        #self.__add_file(img, mask_orig)
+        return img
 
     def add_file_and_mask(self, file):
         
@@ -99,9 +122,7 @@ class CobotLoaderBinary(Dataset):
 
     def __init__(self, root_dir, label, num_labels, transform, 
                  image_size=None, id=-1, create_negative_labels=False,
-                 organ_id=None, organ_name=None, p_neg_img=0.1,
-                 aug_method="none", k_aug=0.0, seed=False):
-
+                 aug_method="none", k_aug=0.0, seed=False,batch_size=1):
         self.root_dir = root_dir
         self.images = []
         self.labels = []
@@ -122,6 +143,23 @@ class CobotLoaderBinary(Dataset):
         self.id = id
         self.num_labels = num_labels
         self.image_size = image_size
+        
+        self.aug_gens=dict()
+        self.aug_method=aug_method
+        self.k_aug=k_aug
+        
+        if seed:
+            np.random.seed(seed)
+        
+        #Batchwise not tested! -- This is only for mutual augmentation within batch.
+        if 'batchwise' in self.aug_method and batch_size >10:
+            print('Dataloader:__init__  Using Batchwise augmentation pairing')
+            self.batch_size=batch_size
+            self.batch_idx=0
+            self.batch_current=[None]*batch_size
+        else:
+            self.batch_size=1
+            print('Dataloader:__init__  Using Global augmentation pairing')
 
         self.files = []
         self.organ_ii = []
@@ -141,11 +179,6 @@ class CobotLoaderBinary(Dataset):
             mask_orig = cv2.imread(mask_file, cv2.IMREAD_GRAYSCALE)
             self.__add_file(img, mask_orig)
 
-        if k_aug > 0 and aug_method == "rand_pair":
-            self.__generate_aug(k_aug, seed)
-
-        print(f"files: {len(self.files)}")
-
     def get_frequency(self):
         return self.num_bg_pixels, self.num_pixels
 
@@ -155,6 +188,15 @@ class CobotLoaderBinary(Dataset):
     def __getitem__(self, idx):
         img = self.images[idx]
         mask = self.labels[idx]
+        do_aug=False
+        if self.k_aug > 0:
+            #Choose augmentation with probability k for each sample
+            do_aug=np.random.choice((False,True),self.batch_size,(1-self.k_aug,self.k_aug))
+        
+        
+        if do_aug:
+            if 'rand_pair' in self.aug_method:
+                img=self.__generate_aug_single(idx)
 
         if self.create_negative_labels:
             masks = []
@@ -189,6 +231,7 @@ class CobotLoaderBinary(Dataset):
 
             img = transformed["image"]
             mask = transformed["mask"]
-      
+            
+        print('Dataloader::Getitem/Index,Aug',idx,do_aug)
         return img, mask, self.label
         
